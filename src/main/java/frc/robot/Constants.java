b9ca03c964f3c39821fe0e25f6504bcae9d5a632
2145Hazmats <hazmatsocial@gmail.com>
2024-07-16 21:08:11 -0400

    initial

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
+
+public final class Constants {
+
+    public static class OperatorConstants {
+        public static final int DRIVER_CONTROLLER = 0;  
+        public static final int OPERATOR_CONTROLLER = 1;  
+    }
+
+    public static class DrivetrainConstants {
+        public static final int MOTOR_LEFT_ID = 2;
+        public static final int MOTOR_RIGHT_ID = 4;
+        public static final double GEAR_RATIO = 1/12.75;
+        public static final double WHEEL_CIRCUMFERENCE = Units.inchesToMeters(6) * Math.PI;
+        public static final double ENCODER_CONVERSION_FACTOR = GEAR_RATIO * WHEEL_CIRCUMFERENCE;
+        public static final double TRACK_WIDTH = Units.inchesToMeters(21.875);
+
+        public static final double DRIVE_P = 0.1;
+        public static final double ANGULAR_P = 0.1;
+    }
+
+    public static class PhotonVisionConstants {
+        //Transform3d from the center of the robot to the camera mount position (ie, robot ➔ camera) in the Robot Coordinate System.
+        public static final Transform3d ROBOT_TO_CAMERA = new Transform3d(0, 0, 0, null);
+        public static final double CAMERA_HEIGHT = 0;
+        public static final double TARGET_HEIGHT = 0;
+        public static final double CAMERA_PITCH_RADIANS = 0;
+        public static final double TARGET_PITCH_RADIANS = 0;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,277 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonUtils;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.PhotonVisionConstants;
+
+public class Drivetrain extends SubsystemBase {
+
+  private DifferentialDrive m_differentialDrive;
+  private DifferentialDriveKinematics m_driveKinematics;
+  private DifferentialDrivePoseEstimator m_driveOdometry;
+
+  private final CANSparkMax leftMotor = new CANSparkMax(DrivetrainConstants.MOTOR_LEFT_ID, MotorType.kBrushless);
+  private final CANSparkMax rightMotor = new CANSparkMax(DrivetrainConstants.MOTOR_RIGHT_ID, MotorType.kBrushless);
+  private RelativeEncoder leftMotorEncoder = leftMotor.getEncoder();
+  private RelativeEncoder rightMotorEncoder = rightMotor.getEncoder();
+
+  private final ADIS16470_IMU gyro = new ADIS16470_IMU();
+
+  private PhotonCamera camera = new PhotonCamera("photonvision");
+  private PhotonPipelineResult result = null;
+  private PhotonTrackedTarget target = null;
+  private PhotonPoseEstimator poseEstimator = new PhotonPoseEstimator(
+      AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo),
+      //Calculates a new robot position estimate by combining all visible tag corners.
+      //Recommended for all teams as it will be the most accurate.
+      //Must configure the AprilTagFieldLayout properly in the UI, please see https://docs.photonvision.org/en/latest/docs/apriltag-pipelines/multitag.html#multitag-localization for more information.
+      PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+      camera,
+      PhotonVisionConstants.ROBOT_TO_CAMERA);
+  private EstimatedRobotPose latestRobotPose = null;
+
+  private PIDController driveController = new PIDController(DrivetrainConstants.DRIVE_P, 0, 0);
+  private PIDController rotController = new PIDController(DrivetrainConstants.ANGULAR_P, 0, 0);
+
+  private final Field2d m_field = new Field2d();
+
+  public Drivetrain() {
+    rightMotorEncoder.setPositionConversionFactor(DrivetrainConstants.ENCODER_CONVERSION_FACTOR);
+    leftMotorEncoder.setPositionConversionFactor(DrivetrainConstants.ENCODER_CONVERSION_FACTOR);
+    rightMotorEncoder.setPosition(0);
+    leftMotorEncoder.setPosition(0);
+
+    leftMotor.setInverted(true);
+    rightMotor.setInverted(false);
+
+    gyro.calibrate();
+    gyro.reset();
+
+    m_differentialDrive = new DifferentialDrive(leftMotor, rightMotor);
+    m_driveKinematics = new DifferentialDriveKinematics(DrivetrainConstants.TRACK_WIDTH);
+    m_driveOdometry = new DifferentialDrivePoseEstimator(
+        m_driveKinematics,
+        new Rotation2d(gyro.getAngle()),
+        0,
+        0,
+        new Pose2d()
+    );
+    // We can implement the standard deviation of vision to make pose estimation more accurate using a Matrix<N3, N1>, but it is complicated.
+
+    SmartDashboard.putData("Field", m_field);
+  }
+
+  public double getGyroAngle() {
+    return gyro.getAngle();
+  }
+
+  public Rotation2d getGyroRotation2d() {
+    return new Rotation2d(Units.degreesToRadians(gyro.getAngle()));
+  }
+
+  public Pose2d getPose2d() {
+    return m_driveOdometry.getEstimatedPosition();
+  }
+  
+  public Command drive(DoubleSupplier leftY, DoubleSupplier rightX, DoubleSupplier rightY, Supplier<String> driveType) {
+    return Commands.run(() -> {
+      switch (driveType.get()) {
+        case "arcadeDrive":
+          m_differentialDrive.arcadeDrive(leftY.getAsDouble(), rightX.getAsDouble());
+          break;
+        case "tankDrive":
+          m_differentialDrive.tankDrive(leftY.getAsDouble(), rightY.getAsDouble());
+          break;
+        case "curvatureDrive1":
+          m_differentialDrive.curvatureDrive(leftY.getAsDouble(), rightX.getAsDouble(), false);
+          break;
+        case "curvatureDrive2":
+          m_differentialDrive.curvatureDrive(leftY.getAsDouble(), rightX.getAsDouble(), true);
+          break;
+      }
+      SmartDashboard.putNumber("Controller Left Y", leftY.getAsDouble());
+      SmartDashboard.putNumber("Controller Right X", rightX.getAsDouble());
+      SmartDashboard.putNumber("Controller Right Y", rightY.getAsDouble());
+      SmartDashboard.putString("Selected Drive Type", driveType.get());
+    }, this);
+  }
+
+  public Command rotateToTarget(DoubleSupplier leftY, DoubleSupplier rightX) {
+    return Commands.run(
+        () -> {
+          // Update Vision results
+          result = camera.getLatestResult();
+          // Get the current best target
+          target = result.getBestTarget();
+
+          if (result.hasTargets()) {
+            m_differentialDrive.arcadeDrive(
+                leftY.getAsDouble(),
+                -rotController.calculate(result.getBestTarget().getYaw(), 0)
+            );
+            SmartDashboard.putNumber("Rot Offset", result.getBestTarget().getYaw());
+            SmartDashboard.putNumber("Vision Rot Speed", -rotController.calculate(result.getBestTarget().getYaw(), 0));
+          }
+          else {
+            m_differentialDrive.arcadeDrive(
+                leftY.getAsDouble(),
+                rightX.getAsDouble()
+            );
+          }
+        },
+        this
+    );
+  }
+
+  public Command driveToTarget(DoubleSupplier leftY, DoubleSupplier rightX, double desiredRange) {
+    return Commands.run(
+        () -> {
+          // Update Vision results
+          result = camera.getLatestResult();
+          // Get the current best target
+          target = result.getBestTarget();
+
+          if (result.hasTargets()) {
+            double range = PhotonUtils.calculateDistanceToTargetMeters(
+              PhotonVisionConstants.CAMERA_HEIGHT,
+              PhotonVisionConstants.TARGET_HEIGHT,
+              PhotonVisionConstants.CAMERA_PITCH_RADIANS,
+              Units.degreesToRadians(result.getBestTarget().getPitch())
+            );
+            m_differentialDrive.arcadeDrive(
+                driveController.calculate(range, desiredRange),
+                rightX.getAsDouble()
+            );
+            SmartDashboard.putNumber("Distance To Target", range);
+            SmartDashboard.putNumber("Desired Range", desiredRange);
+            SmartDashboard.putNumber("Vision Drive Speed", driveController.calculate(range, desiredRange));
+          }
+          else {
+            m_differentialDrive.arcadeDrive(
+                leftY.getAsDouble(),
+                rightX.getAsDouble()
+            );
+          }
+        },
+        this
+    );
+  }
+
+  public Command driveRotateToTarget(DoubleSupplier leftY, DoubleSupplier rightX, double desiredRange) {
+    return Commands.run(
+        () -> {
+          // Update Vision results
+          result = camera.getLatestResult();
+          // Get the current best target
+          target = result.getBestTarget();
+
+          if (result.hasTargets()) {
+            double range = PhotonUtils.calculateDistanceToTargetMeters(
+              PhotonVisionConstants.CAMERA_HEIGHT,
+              PhotonVisionConstants.TARGET_HEIGHT,
+              PhotonVisionConstants.CAMERA_PITCH_RADIANS,
+              Units.degreesToRadians(result.getBestTarget().getPitch())
+            );
+            m_differentialDrive.arcadeDrive(
+                driveController.calculate(range, desiredRange),
+                -rotController.calculate(result.getBestTarget().getYaw(), 0)
+            );
+            SmartDashboard.putNumber("Distance To Target", range);
+            SmartDashboard.putNumber("Desired Range", desiredRange);
+            SmartDashboard.putNumber("Vision Drive Speed", driveController.calculate(range, desiredRange));
+            SmartDashboard.putNumber("Rot Offset", result.getBestTarget().getYaw());
+            SmartDashboard.putNumber("Vision Rot Speed", -rotController.calculate(result.getBestTarget().getYaw(), 0));
+          }
+          else {
+            m_differentialDrive.arcadeDrive(
+                leftY.getAsDouble(),
+                rightX.getAsDouble()
+            );
+          }
+        },
+        this
+    );
+  }
+
+  public void addVisionPose2d(Pose2d visionPose2d, double timestampSeconds) {
+    m_driveOdometry.addVisionMeasurement(visionPose2d, timestampSeconds); //Timer.getFPGATimestamp());
+  }
+
+  /*public Pose2d getVisionPose2D() {
+    // Calculate robot's field relative pose
+    return Pose2d robotPose = PhotonUtils.estimateFieldToRobot(
+        PhotonVisionConstants.CAMERA_HEIGHT,
+        PhotonVisionConstants.TARGET_HEIGHT,
+        PhotonVisionConstants.CAMERA_PITCH,
+        PhotonVisionConstants.TARGET_PITCH,
+        Rotation2d.fromDegrees(-target.getYaw()),
+        getGyroRotation2d(),
+        targetPose,
+        cameraToRobot
+    );
+  }*/
+
+  @Override
+  public void periodic() {
+    // Update Vision results
+    result = camera.getLatestResult();
+
+    // Try to update latestRobotPose with a new EstimatedRobotPose and add the vision Pose2d to drivetrains odometry
+    try {
+      latestRobotPose = poseEstimator.update(result).get();
+      addVisionPose2d(latestRobotPose.estimatedPose.toPose2d(), latestRobotPose.timestampSeconds);
+    } catch (Exception e) { // catch = catching an exception, java.util.Optional.get() throws NoSuchElementException if no value is present
+      latestRobotPose = null;
+    }
+
+    m_driveOdometry.updateWithTime(
+        Timer.getFPGATimestamp(),
+        new Rotation2d(Units.degreesToRadians(gyro.getAngle())),
+        -leftMotorEncoder.getPosition(),
+        -rightMotorEncoder.getPosition()
+    );
+    
+    SmartDashboard.putNumber("Rot", gyro.getAngle());
+    SmartDashboard.putNumber("Bot X", getPose2d().getX());
+    SmartDashboard.putNumber("Bot Y", getPose2d().getY());
+    if (latestRobotPose != null) { SmartDashboard.putNumber("Vision Rot", latestRobotPose.estimatedPose.getRotation().getAngle()); }
+    if (latestRobotPose != null) { SmartDashboard.putNumber("Vision X", latestRobotPose.estimatedPose.getY()); }
+    if (latestRobotPose != null) { SmartDashboard.putNumber("Vision Y", latestRobotPose.estimatedPose.getY()); }
+    if (latestRobotPose != null) { SmartDashboard.putNumber("Vision Timestamp", latestRobotPose.timestampSeconds); }
+    m_field.setRobotPose(m_driveOdometry.getEstimatedPosition());
+  }
+}
